3aac7a7a17e0fe1f207fe005d9d6199be341be66
mmooney512 <31974098+mmooney512@users.noreply.github.com>
2025-01-15 10:40:39 +0100

    Update main.py

--- python /connect_to_sFTP/main.py
+++ python /connect_to_sFTP/main.py
@@ -1,1 +1,19 @@
-
+# -- LIBRARY -----------------------------------------------------------------
+import sys
+ 
+# -- LOCAL FILES -------------------------------------------------------------
+from src.file_upload import FileUpload
+ 
+def main(argv) -> None:
+    
+    file_list = ['file_one.txt', 'file_two.txt']
+    
+    # upload files to sftp
+    if len(file_list) > 0:
+        fp = FileUpload()
+        fp.send_files(file_list=file_list)
+ 
+ 
+# Start the program
+if __name__ == '__main__':
+    main(sys.argv[1:])
